8bd52ba2d11df8b4f947f62a2c2dd7d286b6096a
lucapu88 <caputoluca88@gmail.com>
2019-11-29 15:21:00 +0100

    prima fase: con each diventano rossi i quadratini che ho scelto

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
 // Suggerimento: usare una classe per individuare quali quadratini diventano rossi
 // BONUS: generare la griglia dei quadratini utilizzando jQuery
+
+$(document).ready(function() {
+  $('.red').click(function(){
+    $('.quadrato').each(function(){
+      $(this).addClass('active');
+    });
+  });
+});
