520bf82b34e48c71623181a19d9fd85d35fa9e9e
lucapu88 <caputoluca88@gmail.com>
2019-11-29 13:24:31 +0100

    inizio

---
+++ main.js
@@ -0,0 +1,4 @@
+// Creare in HTML una griglia formata da 8x8 quadratini tutti bianchi.
+// 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
+// Suggerimento: usare una classe per individuare quali quadratini diventano rossi
+// BONUS: generare la griglia dei quadratini utilizzando jQuery
