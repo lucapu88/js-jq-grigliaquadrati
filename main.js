d36e79c585db19cb1765968b2354025cf7b96094
lucapu88 <caputoluca88@gmail.com>
2019-11-29 14:36:13 +0100

    creato griglia 8X8

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Creare in HTML una griglia formata da 8x8 quadratini tutti bianchi.
+// Creare in HTML una griglia formata da 8x8 quadratini tutti bianchi. ok
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
 // Suggerimento: usare una classe per individuare quali quadratini diventano rossi
 // BONUS: generare la griglia dei quadratini utilizzando jQuery
