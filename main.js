6b0f407afc7732c2b7a6f1b9af762be9199b51fa
lucapu88 <caputoluca88@gmail.com>
2019-11-29 17:49:25 +0100

    aggiunto un ciclo per generare bombe. sembra funzioni

--- main.js
+++ main.js
@@ -4,8 +4,26 @@
 // BONUS: generare la griglia dei quadratini utilizzando jQuery
 
 $(document).ready(function() {
+  var bombeGenerate = []; //creo un contenitore di bombe
+  var contatore = 0;
+  while (bombeGenerate.length < 15) {
+   var bombe = generaRandom(1,64); //Creo un numero causale
+   if(!bombeGenerate.includes(bombe)) { //Controllo che il numero non ci sia nell’Array
+         bombeGenerate.push(bombe); //Se non c’è lo metto nell’array (bombeGenerate)
+   }
+   contatore++
+  }
+  for (var i = 0; i < bombeGenerate.length; i++) {
+    $('.quadrato').eq(bombeGenerate[i]).addClass('red');
+  }
+
+
   $('.quadrato').click(function(){ // al click sul quadrato
       $(this).addClass('active'); // viene aggiunta una classe active
   });
-  
 });
+
+
+function generaRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
